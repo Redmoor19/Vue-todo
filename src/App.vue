f84e8861616e91acaad0d06d0b0040869c8cfc49
Danila Mandrabura <danilamandrabura@gmail.com>
2024-09-11 16:24:37 +0300

    batman

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import TodoForm from "./components/TodoForm.vue";
+
+type Todo = {
+  id: number;
+  title: string;
+  checked: boolean;
+};
+
+const todos = ref<Todo[]>([]);
+</script>
+
+<template>
+  <div class="h-screen">
+    <TodoForm />
+  </div>
+</template>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const todo = ref("");
+</script>
+
+<template>
+  <div
+    class="bg-gradient-to-br from-blue-500 to-emerald-300 flex items-center p-5 gap-10"
+  >
+    <input class="w-96 rounded h-8 px-3" v-model.trim.lazy="todo" />
+    <button
+      @click="console.log(todo)"
+      class="bg-green-500 rounded py-2 px-3 text-white"
+    >
+      Add todo
+    </button>
+  </div>
+</template>
